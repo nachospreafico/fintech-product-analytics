655819bb22ed0a083f090a755e72ce2feec9def8
Ignacio Spreafico <nachospreafico06@gmail.com>
2025-12-27 00:27:48 +0100

    added README file

--- sql/vw_transaction_analytics.sql
+++ sql/vw_transaction_analytics.sql
@@ -9,15 +9,3 @@ SELECT
 	SUM(fee_amount_eur) AS total_fee_revenue
 FROM transactions
 GROUP BY user_id, DATETRUNC(MONTH, transaction_date);
-
--- Purpose: Aggregate transactions to user–month grain
--- Used for: Active Users, Transactions, Fee Revenue
-CREATE VIEW vw_subscription_revenue AS
-SELECT
-	s.user_id,
-	DATETRUNC(MONTH, s.month) AS subscription_month,
-	p.plan_name,
-	p.monthly_fee_eur AS subscription_fee_eur
-FROM subscriptions s
-JOIN dim_plans p
-ON s.plan_id = p.plan_id
